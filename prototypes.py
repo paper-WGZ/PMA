10450a1ae5ceee386fb85c7738fb0bb5f39d0a1c
WGZ <110165687+paper-WGZ@users.noreply.github.com>
2025-06-16 10:22:02 +0800

    Add files via upload

--- prototypes.py
+++ prototypes.py
@@ -3,210 +3,203 @@ import copy
 import torch
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-from datetime import datetime
 
-from PMA0.dPMA import *
-from PMA0 import args as args_pma
-
-class ACluster(SplitSamples):
-    def __init__(self, sample: ASample):
-        self.series = sample.series
-        self.num, self.num_var, self.seq_len = self.series.shape
-        self.split = sample.split
-        self.num_sgm = sample.num_sgm
-        self.segments = sample.segments
-        self.information = sample.information
-
-    def append(self, sample: ASample):
-        self.num += sample.num
-        self.series = torch.cat((self.series, sample.series), dim=0)
-        self.split += sample.split
-        self.num_sgm += sample.num_sgm
-        self.segments += sample.segments
-        self.information += sample.information
-
-class Clusters:
-    def __init__(self, proto_init: SplitSamples):
-        self.prototypes = proto_init
-        self.samples = [ACluster(copy.deepcopy(proto_init[i]))
-                        for i in range(proto_init.num)]
-
-    def append(self, sample: ASample):
-        self.prototypes.append(copy.deepcopy(sample))
-        self.samples.append(ACluster(copy.deepcopy(sample)))
+from split import *
+from match import *
+from fine_tuning import *
+from alignment import *
 
 
+class object_sample:
+    def __init__(self, series:torch.Tensor, label, args):
+        self.seq_len = len(series)
+        self.Series = series    # raw series
+        self.Label = label  # label
+        self.Split = GetSplit(series, args)    # segment point list
+        self.num_seg = len(self.Split) - 1   # number of segments
+        self.Segment, self.Angle = SplitSeries(series, self.Split)
 
-def init_prototypes(samples, labels, proto_init):
+        self.idx_proto = 0
+        self.Match = [-1] * self.num_seg
+
+
+    def update_by_seg(self, segment):
+        self.Segment = segment
+        self.Series = MergeSegments(segment)
+        self.seq_len = len(self.Series)
+
+        self.Split[1] = len(segment[0])
+        for k in range(self.num_seg):
+            sgm = segment[k]
+            index = torch.arange(0, len(sgm)) / self.seq_len
+            self.Angle[k] = slant_angle(index, sgm)
+            if k >= 2:
+                self.Split[k] = self.Split[k -1] + len(segment[k - 1]) - 1
+
+
+class SplitSamples:
+    def __init__(self, samples: torch.Tensor, split, segments, information):
+        """
+        sample: (batch_size, num_var, len_seq)
+        """
+        self.series = samples
+        self.num, self.num_var, self.seq_len = samples.shape
+
+        self.split = split
+        # split: [batch_size, num_var, num_seg[i][j] + 1]
+
+        self.num_sgm = [[len(s) - 1 for s in sp] for sp in self.split]  # number of segments
+        # num_sgm: [batch_size, num_var]
+
+        self.segments, self.information = segments, information
+        # segments: [batch_size, num_var, num_seg[i][j], tensor()]
+        # information: [batch_size, num_var, tensor(5, num_seg[i][j])],
+        # angle, phase_median, phase_range, magnit_median, magnit_range
+
+    def __getitem__(self, i):
+        samples = self.series[i].unsqueeze(0)
+        split = [self.split[i]]
+        segments = [self.segments[i]]
+        information = [self.information[i]]
+        return SplitSamples(samples, split, segments, information)
+
+
+class MatchSamples(SplitSamples):
+    def __init__(self, samp: SplitSamples, path):
+        """
+        samp: A sample
+        path: [1, num_var, num_match]
+        """
+        super().__init__(samp.series, samp.split, samp.segments, samp.information)
+        self.path = path
+        self.alignment = []
+        self.d_shape, self.d_phase, self.d_magnit = torch.tensor([[], [], []], dtype=torch.float)
+
+
+    def append(self, samp: SplitSamples, path):
+        """
+        samp: A sample
+        path: [1, num_var, num_match]
+        """
+        self.num += samp.num
+        self.series = torch.cat((self.series, samp.series), dim=0)
+        self.split += samp.split
+        self.num_sgm += samp.num_sgm
+        self.segments += samp.segments
+        self.information += samp.information
+        self.path += path
+
+    def update_by_split(self, split):
+        self.split = split
+        self.num_sgm = [[len(s) - 1 for s in sp] for sp in split]
+        self.segments, self.information = batch_info(self.series, split)
+
+
+class Prototypes:
+    def __init__(self, samp: SplitSamples, label=0):
+        """
+        samp: A sample
+        """
+        self.label = label
+        self.num = 0
+        self.num_var, self.seq_len = samp.num_var, samp.seq_len
+        self.series = []
+        self.split = []
+        self.num_sgm = []
+        self.segments = []
+        self.information = []
+        self.match_samples = []
+
+        self.append(samp)
+
+    def append(self, samp: SplitSamples):
+        """
+        samp: A sample
+        """
+        # prototypes
+        self.num += samp.num
+        self.series += samp.series
+        self.split += samp.split
+        self.num_sgm += samp.num_sgm
+        self.segments += samp.segments
+        self.information += samp.information
+
+        # matched samples
+        path = [[[(k, k) for k in range(samp.num_sgm[0][j])]
+                 for j in range(samp.num_var)]]
+        self.match_samples.append(MatchSamples(samp, path))
+
+    def __getitem__(self, i):
+        samples = self.series[i].unsqueeze(0)
+        split = [self.split[i]]
+        segments = [self.segments[i]]
+        information = [self.information[i]]
+        return SplitSamples(samples, split, segments, information)
+
+
+
+
+def init_prototypes(samples, args, label=0):
     """
-    :param samples: SplitSamples
-    :param proto_init: SplitSamples
+
+    :param samples: [[split_sample,..., split_sample], ...,[...]]
+    :param match_window:
+    :param match_thres:
+    :param max_unmatch:
+    :param max_dist:
     :return:
     """
 
-    clusters = Clusters(proto_init)
-    label_set = [[i] for i in range(proto_init.num)]
+    prototypes = Prototypes(copy.deepcopy(samples[0]), label)
 
-    for i in range(samples.num):
+    for i in range(1, samples.num):
         samp = samples[i]
+        path, unmatch = Match_batch(samp, prototypes, args)
 
-        match_path, rate_mismatch, samp_match, proto_match = get_match(copy.deepcopy(samp), proto_init)
-        opt = min(range(proto_init.num), key=lambda i: sum(rate_mismatch[i]))
+        opt = min(range(prototypes.num), key=lambda i: sum(unmatch[i]))
 
-        match_path, rate_mismatch, samp_match, proto_match = [match_path[opt]], rate_mismatch[opt], samp_match[opt], proto_match[opt]
-        proto_opt = proto_init[opt]
+        if sum(unmatch[opt]) > args.thres_unmatch:
+            prototypes.append(copy.deepcopy(samp))
+        else:
+            prototypes.match_samples[opt].append(samp, [path[opt]])
 
-        aligned_segments, _, original_information, align_path = get_align(samp_match, proto_match, proto_opt)
-        aligned_samp = ASample(samp.series, proto_opt.split, aligned_segments, original_information)
-
-        clusters.samples[opt].append(aligned_samp)
-        label_set[opt].append(labels[i])
-
-        if args_pma.is_plot:
-            root_plot = f"{args_pma.root}{args_pma.data_name}/figure/"
-            plot_align(samp, proto_opt, match_path, samp_match, proto_match,
-                       align_path, aligned_segments, root_plot)
-
-    return clusters, label_set
+    return prototypes
 
 
-def adjust_segment1(sgm, t_range, seq_len, v_median, v_range):
-    """
-    :sgm: raw segment
+def update_prototypes(prototypes):
+    for i in range(prototypes.num):
+        proto = prototypes[i]
+        samp = prototypes.match_samples[i]
+        path = samp.path
 
-    """
-    len_new = round((seq_len-1) * t_range.item())+1
-    t = torch.linspace(0, t_range, len_new, device=sgm.device)
-    trend_new = v_range / t_range * t + v_median - v_range / 2
+        samp, proto, d_phase, d_magnit = Fine_batch(samp, proto, path)
+        align, d_shape = Align_batch(samp, proto)
+        # align: [num_sample, num_var, num_seg, tensor()]
 
-    sgm = interp_resamp(sgm, len_new)
-    slope, intercept = linear_fit(t, sgm)
-    trend_old = slope * t + intercept
+        samp.alignment = align
+        samp.d_phase = d_phase
+        samp.d_magnit = d_magnit
+        samp.d_shape = d_shape
 
-    sgm = sgm - trend_old + trend_new
-    return sgm
+        for j in range(proto.num_var):
+            for k in range(proto.num_sgm[0][j]):
+                all_sgm = [align[s][j][k] for s in range(samp.num)]
+                average_sgm = torch.mean(torch.stack(all_sgm, dim=0), dim=0)
 
-def adjust_segment(sgm, len_seq, v_median, v_range):
-    """
-    :sgm: raw segment
+                # information: [num_sample, num_var, tensor(5, num_seg[i][j])], angle, phase_median, phase_range, magnit_median, magnit_range
+                all_info = [samp.information[s][j][:, k] for s in range(samp.num)]
+                average_info = torch.mean(torch.stack(all_info, dim=1), dim=1)
 
-    """
-    t = torch.linspace(0, 1, len_seq, device=sgm.device)[:len(sgm)]
-    trend_new = v_range / t[-1] * t + v_median - v_range / 2
+                t_range, v_median, v_range = average_info[2:]
+                sgm = adjust_segment(average_sgm, t_range, proto.seq_len, v_median, v_range)
 
-    slope, intercept = linear_fit(t, sgm)
-    trend_old = slope * t + intercept
+                prototypes.segments[i][j][k] = sgm
+                prototypes.information[i][j][:, k] = average_info
 
-    sgm = sgm - trend_old + trend_new
-    return sgm
-
-def MergeSegments(segment):
-    series = segment[0]
-    for k in range(1, len(segment)):
-        p_l, p_r = series[-1], segment[k][0]
-        segment[k] = segment[k] - p_r + p_l
-        series = torch.cat([series, segment[k][1:]])
-    series = normalize(series, mode='min-max')
-    return series
-
-def update_prototypes(clusters):
-    prototypes = clusters.prototypes
-    samples = clusters.samples
-
-    for p in range(prototypes.num):
-        samp = samples[p]  # ACluster
-        '''for i in range(samp.num):
-            print(p, i, samp.num, samp.series.shape, len(samp.segments))
-            print(p,i,samp.num, len(samp.segments[i]))
-            len_sgm = [len(samp.segments[i][0][k]) for k in range(len(samp.segments[i][0]))]
-            print(p,i,len_sgm)'''
-        for j in range(prototypes.num_var):
-            segment = []
-            for k in range(prototypes.num_sgm[p][j]):
-                sum_sgm = torch.zeros_like(prototypes.segments[p][j][k])
-                sum_info = torch.zeros_like(prototypes.information[p][j][:, k])
-                for i in range(samp.num):
-                    '''print(p,i,k)
-                    len_sgm = [len(samp.segments[i][0][k]) for k in range(len(samp.segments[i][0]))]
-                    print(len_sgm)'''
-                    sum_sgm += samp.segments[i][j][k]
-                    sum_info += samp.information[i][j][:, k]
-                # 平均子序列
-                avg_sgm = sum_sgm / samp.num
-                avg_info = sum_info / samp.num  # tensor(5): angle, p_median, p_range, m_median, m_range
-                _, _, t_range, v_median, v_range = avg_info
-                sgm = adjust_segment(avg_sgm, prototypes.seq_len, v_median, v_range)
-                segment.append(sgm)
-                prototypes.segments[p][j][k] = sgm
-
-                '''# 子序列信息
-                index = torch.linspace(0, (len(sgm) - 1) / (prototypes.seq_len - 1), len(sgm), device=prototypes.series.device)
-                info = sgm_info(index, sgm)
-                prototypes.information[p][j][:, k] = info'''
-
-            # 合并子序列
-            series = MergeSegments(segment)
-            prototypes.series[p][j] = series
-
-    prototypes = SplitSamples(prototypes.series, prototypes.split)
-
-    return clusters
-
-
-
-def evaluate_prototypes_pma(clusters):
-    d_shape, d_phase, d_magnit, num = 0, 0, 0, 0
-    prototypes = clusters.prototypes
-    samples = clusters.samples
-    for p in range(prototypes.num):
-        proto = prototypes[p]  # ASample
-        samp = samples[p]  # ACluster
-        num += samp.num
-
-        distances = pma_sp(samp, proto)[1]
-        for i in range(len(distances)):
-            for j in range(len(distances[i])):
-                for k in range(len(distances[i][j])):
-                    _, d_pm, d_pr, d_mm, d_mr, d_s = distances[i][j][k]
-                    d_shape += d_s
-                    d_phase += (d_pm + d_pr)
-                    d_magnit += (d_mm + d_mr)
-
-    return d_shape, d_phase, d_magnit, num
-
-
-def plot_clusters(clusters, centroids, init, labels, path, title='0'):
-    colors = [(0.4, 0.7, 1), (0.6, 0.9, 0.6), 'orange']
-    print(labels)
-    for p in range(len(centroids)):
-        fig, ax = plt.subplots(figsize=(12, 8))
-
-        for i, series in enumerate(clusters[p]):
-            ax.plot(series[0], linewidth=2, color=colors[labels[p][i]],
-                    alpha=1)
-
-        ax.plot(centroids[p][0], linewidth=4, color='black')
-        ax.plot(init[p][0], linewidth=4, color='red')
-
-        plt.tick_params(axis='both', which='major', labelsize=18)
-
-        # 保存图像
-        time = datetime.now().strftime('%d%H%M%S%f')
-        plt.savefig(f'{path}/{title}_{p}.svg')
-        plt.close()
+    return prototypes
 
 
 def get_prototypes(samples, labels, args):
-    """
-    input:
-    samples: Tensor(num, var, seq)
-    labels: Tensor()
-
-    output:
-    prototype_set: [ACluster] * num_class
-    label_set: [] * num_class
-    sample_set: [[ACluster] * num_proto] * num_class
-    """
     num_samples, num_var, seq_len = samples.shape
     labels = labels.tolist()
 
@@ -221,91 +214,30 @@ def get_prototypes(samples, labels, args):
             sample_set.append([samples[i]])
 
     prototype_set = []
-
-
-    time = datetime.now().strftime('%d%H%M%S%f')
-    path = f'./figure/{args.data_name}pma_{time}'
-    os.makedirs(path, exist_ok=True)
-
     for c in range(len(label_set)):
         samp = torch.stack(sample_set[c], dim=0)
-        samp_sp = get_split(samp)[0]
-        samp = SplitSamples(samp, samp_sp)
-
-        clusters = init_prototypes(samp, args.thres_mismatch)
-        clusters = update_prototypes(clusters)
-
-        prototype_set.append(clusters.prototypes)
+        samp = split_batch(samp, args)
+        proto = init_prototypes(samp, args, label_set[c])
+        proto = update_prototypes(proto)
+        prototype_set.append(proto)
         sample_set[c] = samp
-
-        protos = clusters.prototypes.series
-        samps = [clusters.samples[i].series for i in range(len(protos))]
-        plot_clusters(samps, protos, path, f'{c}')
-
 
     return prototype_set, label_set, sample_set
 
 
-def get_prototypes_all(samples, labels, args):
-    num_samples, num_var, seq_len = samples.shape
-    labels = labels.tolist()
+def evaluate_prototypes(prototype_set):
+    d_shape, d_phase, d_magnit = 0, 0, 0
+    num_sample = 0
+    for prototypes in prototype_set:
+        for i in range(prototypes.num):
+            match_samples = prototypes.match_samples[i]
+            num_sample += match_samples.num
 
-    # 按类别分组
-    label_set, sample_set = [], []
-    for i in range(num_samples):
-        if labels[i] in label_set:
-            idx = label_set.index(labels[i])
-            sample_set[idx].append(samples[i])
-        else:
-            label_set.append(labels[i])
-            sample_set.append([samples[i]])
+            d_shape += torch.sum(match_samples.d_shape).item()
+            d_phase += torch.sum(match_samples.d_phase).item()
+            d_magnit += torch.sum(match_samples.d_magnit).item()
 
-    proto_init = [sample_set[0][1], sample_set[1][2]]
-    proto_init = torch.stack(proto_init, dim=0)
-    proto = copy.deepcopy(proto_init)
-    proto_sp = get_split(proto)[0]
-    proto = SplitSamples(proto, proto_sp)
-
-    samp_sp = get_split(samples)[0]
-    samp = SplitSamples(samples, samp_sp)
-
-    clusters_set, label_set = init_prototypes(samp, labels, proto)
-    clusters_set = update_prototypes(clusters_set)
-
-    protos = clusters_set.prototypes.series
-    samps = [clusters_set.samples[i].series for i in range(len(protos))]
-
-    time = datetime.now().strftime('%d%H%M%S%f')
-    path = f'D:/备份/pma res/prototypes/{args.data_name}_pma{time}'
-    os.makedirs(path, exist_ok=True)
-    plot_clusters(samps, protos, proto_init, label_set, path)
-
-    eva = evaluate_prototypes_pma(clusters_set)
-
-    return clusters_set, label_set, eva
-
-
-
-
-def evaluate_prototypes(clusters, centroids):
-    d_shape, d_phase, d_magnit, num = 0, 0, 0, 0
-
-    for p in range(len(centroids)):
-        proto = centroids[p].unsqueeze(0)
-        samp = clusters[p]
-        distances = pma(samp, proto)[1]
-        num += len(samp)
-        for i in range(len(distances)):
-            for j in range(len(distances[i])):
-                for k in range(len(distances[i][j])):
-                    _, d_pm, d_pr, d_mm, d_mr, d_s = distances[i][j][k]
-                    d_shape += d_s
-                    d_phase += (d_pm + d_pr)
-                    d_magnit += (d_mm + d_mr)
-        #print(d_shape, d_phase, d_magnit, num)
-    return d_shape, d_phase, d_magnit, num
-
-
+    return d_shape / num_sample, d_phase / num_sample, d_magnit / num_sample
 
 
 def plot_prototypes(prototype_set, plot_path):
@@ -372,5 +304,3 @@ def plot_samples(prototype_set, sample_set, args, plot_path):
             os.makedirs(plot_path, exist_ok=True)
             plt.savefig(os.path.join(plot_path, f'{c}_{i}.png'))
             plt.close()
-
-
